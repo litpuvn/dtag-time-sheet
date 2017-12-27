ea98316871cdd90b588c0705e9b9a75075e5b9f6
hieutran <dtag.dev1@gmail.com>
2017-12-27 14:19:52 +0700

    show percent

--- js/kpi.js
+++ js/kpi.js
@@ -50,6 +50,10 @@ app.controller('kpiCtrl', function($scope) {
                 borderWidth: 1,
                 shadow: false
             },
+            tooltip: {
+                pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.percentage:.0f}%)<br/>',
+                shared: true
+            },
             plotOptions: {
                 column: {
                     stacking: 'normal',
